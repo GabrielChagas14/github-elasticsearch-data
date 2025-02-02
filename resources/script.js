922ffbe125f9050d34df467192ec9c531bc946e2
GabrielChagas14 <gmelo123455@gmail.com>
2025-02-02 16:55:52 -0300

    update style.css and sripts.js

--- resources/script.js
+++ resources/script.js
@@ -16,6 +16,31 @@ async function fetchIssues() {
             `;
             tableBody.innerHTML += row;
         });
+
+
+
+        let regressionTests = issues.filter(issue => issue.related_topic === 'Regression Testing').length;
+        let refactoring = issues.filter(issue => issue.related_topic === 'Refactoring').length;
+        let both = issues.filter(issue => issue.related_topic === 'Both').length;
+        let none = issues.filter(issue => issue.related_topic === 'None').length;
+
+        const ctx = document.getElementById('myChart').getContext('2d');
+        new Chart(ctx, {
+            type: 'pie', // Pode ser 'bar', 'line', 'doughnut', etc.
+            data: {
+                labels: ['Teste de Regressão', 'Refatoração', 'Ambas', 'Outros'],
+                datasets: [{
+                    label: 'Quantidade de Issues',
+                    data: [regressionTests, refactoring, both, none],
+                    backgroundColor: ['#fed10a', '#343741', '#00bfb3', '#0077cc'],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true
+            }
+        });
+
     } catch (error) {
         console.error('Erro ao carregar issues:', error);
     }
